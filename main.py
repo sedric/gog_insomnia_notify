e8d08f22d29783512978479c9b147f921a8e2be6
sedric <sedric+github@sedric.fr>
2016-03-21 22:01:58 +0100

    - Export config - Logging - Use external command on wishlisted item

--- main.py
+++ main.py
@@ -4,8 +4,20 @@
 import urllib2
 import json
 import re
+import logging
 from time import sleep
 from subprocess import call
+import ConfigParser
+
+def get_dict_from_config(conffd):
+  config = {}
+
+  for section in conffd.sections():
+    config[section] = {}
+    for option in conffd.options(section):
+      data = conffd.get(section, option)
+      config[section][option] = data
+  return config
 
 def search(game, wishlist):
     fdwishes = open(wishlist, 'r')
@@ -23,28 +35,28 @@ def get_currency_id(cc, groups):
             group_possible.append(str(group[0]))
     return group_possible
 
-def print_infos(cmd, game, oprice, nprice, discount, gameurl):
+def notify(cmd, game, oprice, nprice, discount, gameurl):
     message =  game + "\n"
     message = message +  "  Old price : " + oprice + "\n"
     message = message +  "  New price : " + nprice + "\n"
     message = message +  "  Discount  : " + discount + "%\n"
     message = message +  "  https://gog.com" + gameurl + "\n"
-    print message
+    call([cmd, message])
 
-def processing(my_country_code, my_currency, wishlist, cmd, data):
+def processing(config, data):
     # Extract data from JSON
     dealtype    = data['dealType']
     deal        = data[dealtype]
     gameurl     = deal['url']
     game        = gameurl.split("/")[2]
     countrygrp  = deal['prices']['countriesGroups']
-    currency_ids = get_currency_id(my_country_code, countrygrp)
+    currency_ids = get_currency_id(config['my_country_code'], countrygrp)
 
     # Because a country can be in multiple groups, here we find the one that
     # match my_currency
     for currency_id in currency_ids:
         try:
-            prices = deal['prices']['groupsPrices'][my_currency][currency_id]
+            prices = deal['prices']['groupsPrices'][config['my_currency']][currency_id]
             break
         except KeyError:
             continue
@@ -52,44 +64,40 @@ def processing(my_country_code, my_currency, wishlist, cmd, data):
     newprice    = prices.split(";")[1]
     discount    = str(data['discount'])
 
-    if search(game, wishlist) == 0:
-        print_infos(cmd, game, oldprice, newprice, str(data['discount']), gameurl)
-    else:
-        print game + " : " + newprice + my_currency + " (-" + discount + "%)"
+    if search(game, config['wishlist']) == 0:
+        notify(config['cmd'], game, oldprice, newprice, discount, gameurl)
+    logging.info(game + " : " + newprice + config['my_currency'] + " (-" + discount + "%)")
 
 def main():
-    # Config
-    url             = 'https://www.gog.com/insomnia/current_deal'
-    # lgogdownloader --wishlist | grep https | cut -d'/' -f 5 > wishlist
-    wishlist        = './wishlist'
-    last_game       = None
-    my_country_code = 'FR'
-    my_currency     = 'EUR'
-    cmd             = 'echo'
+    config    = {}
+    last_game = None
+    conffd    = ConfigParser.ConfigParser()
+    conffd.read("config.ini")
+    config    = get_dict_from_config(conffd)
+    logging.basicConfig(format='%(levelname)s:%(message)s', level=config['insomnia']['loglevel'])
 
     while True:
         try:
-            jsonurl = urllib2.urlopen(url)
+            jsonurl = urllib2.urlopen(config['insomnia']['url'])
             data    = json.loads(jsonurl.read())
-        except urllib2.HTTPError:
-            # The URL does not exists, the promo as ended
+        except urllib2.HTTPError as e:
+            logging.critical("The URL does not exists, the promo as ended")
             exit(1)
-        except urllib2.URLError:
-            # Connexion problem, wait a little time
+        except urllib2.URLError as e:
+            logging.debug("Connexion problem, will try again later " + e)
             sleep(1)
 
         try:
             game = data['product']['url'].split("/")[2]
             if not last_game == game:
-                processing(my_country_code, my_currency, wishlist, cmd, data)
+                print last_game
+                processing(config['insomnia'], data)
                 last_game = game
         except KeyError:
             game = data['bundle']['description']
-            #debug to gain bundle example
-            #call(["./print.sh", game, str(data)])
             discount = str(data['discount'])
             if not last_game == game:
-                print "Bundle : " + game + " (-" + discount + "%)"
+                logging.info("Bundle : " + game + " (-" + discount + "%)")
                 last_game = game
             sleep(10)
         except NameError:
